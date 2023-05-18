ac927f478e59e04e371c96a995d59c380aa50710
axi <76423997+AyeItsAxi@users.noreply.github.com>
2023-05-17 18:07:43 -0700

    Base solution

---
+++ Flare/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Flare
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
