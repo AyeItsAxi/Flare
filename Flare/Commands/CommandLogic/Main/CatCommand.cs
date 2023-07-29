ab8d832377309a1030e1a30cd792de6e6e8f84a3
axi <76423997+AyeItsAxi@users.noreply.github.com>
2023-07-28 19:09:07 -0700

    Unscuff CatCommand.cs.

--- Flare/Commands/CommandLogic/Main/CatCommand.cs
+++ Flare/Commands/CommandLogic/Main/CatCommand.cs
@@ -8,16 +8,9 @@ public static class CatCommand
     public static async Task RunCommandLogic(SocketMessage message)
     {
         var catCommandMsg = await message.Channel.SendMessageAsync("Getting a random cat image for you...");
-        //this is so scuffed bruh
-        //protip: dont put [{ content }], its so annoying
-        //i know im stupid and theres a better way to do it but im out of time and have to go
-        var jsonResponse = new WebClient().DownloadString("https://api.thecatapi.com/v1/images/search");
-        jsonResponse = jsonResponse.Substring(jsonResponse.Length - (jsonResponse.Length - 1));
-        jsonResponse = jsonResponse.TrimEnd(']');
-        MessageBox.Show(jsonResponse);
         var imageEmbed = new EmbedBuilder()
             .WithTitle("Meow")
-            .WithImageUrl(JObject.Parse(jsonResponse)["url"].ToString())
+            .WithImageUrl((string)JArray.Parse(new WebClient().DownloadString("https://api.thecatapi.com/v1/images/search"))[0]["url"])
             .WithFooter($"Random cat for {message.Author.Username}")
             .WithColor(Color.Purple)
             .Build();
